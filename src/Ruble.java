9a85807d5d6f33b5f4d37d918c1fdd22830b5dd9
Inna Smirnova <moipr@yandex.ru>
2020-06-10 20:07:19 +0300

    Added new files

---
+++ src/Ruble.java
@@ -0,0 +1,15 @@
+public class Ruble {
+    public static void main(String[]args) {
+        int customerAmount = 2600;
+        int minLimit = 1000;
+        int bonus = 1;
+        if (customerAmount > minLimit) {
+            bonus = 1;
+        }
+        else {
+            bonus = 0;
+        }
+        int totalBonus = customerAmount / 100;
+        System.out.println(totalBonus);
+    }
+}
